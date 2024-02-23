69936bafeff8e965c3a7877d0944a984e13efa83
asianParadissseee <asian.paradissseee@gmail.com>
2024-02-23 14:01:43 +0600

    add condition for repead additional

--- scripts/form.js
+++ scripts/form.js
@@ -5,8 +5,16 @@ document.getElementById('form').addEventListener('submit', function (e) {
     const phone = document.getElementById('phone').value;
     const sub1 = document.querySelector('input[name="sub1"]').value;
     const phoneRegex = /^\+7\(\d{3}\)\d{3}-\d{2}-\d{2}$/;
+
     if (!phoneRegex.test(phone)) {
         alert('Неверный формат номера телефона. Используйте +7(XXX)XXX-XX-XX');
+        return;
+    }
+
+    const formData = {name: name, phone: phone, sub1: sub1};
+    const formDataJSON = JSON.stringify(formData);
+    if(localStorage.getItem(formDataJSON)) {
+        alert("Вы уже отправляли заявку с этими данными.");
         return;
     }
 
@@ -15,11 +23,13 @@ document.getElementById('form').addEventListener('submit', function (e) {
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({name: name, phone: phone, sub1: sub1})
+        body: formDataJSON
     })
         .then(response => response.json())
         .then(data => {
             if (data.success) {
+                // Сохраняем данные формы в localStorage как "отправленные"
+                localStorage.setItem(formDataJSON, true);
                 window.location.href = "../pages/success.html"
             } else {
                 window.location.href = "../pages/error.html"
